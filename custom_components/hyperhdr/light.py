688c800a2c9d7f322a454bd85332eeedbcc67035
mjoshd <35782597+mjoshd@users.noreply.github.com>
2022-01-23 04:17:40 -0500

    fix camera stream

--- custom_components/hyperhdr/light.py
+++ custom_components/hyperhdr/light.py
@@ -1,11 +1,11 @@
 """Support for HyperHDR-NG remotes."""
 from __future__ import annotations
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Callable, Mapping, Sequence
 import functools
 import logging
 from types import MappingProxyType
-from typing import Any, Callable
+from typing import Any
 
 from hyperhdr import client, const
 
@@ -85,7 +85,7 @@ async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
-) -> bool:
+) -> None:
     """Set up a HyperHDR platform from config entry."""
 
     entry_data = hass.data[DOMAIN][config_entry.entry_id]
@@ -122,7 +122,6 @@ async def async_setup_entry(
             )
 
     listen_for_instance_updates(hass, config_entry, instance_add, instance_remove)
-    return True
 
 
 class HyperHDRBaseLight(LightEntity):
@@ -240,12 +239,12 @@ class HyperHDRBaseLight(LightEntity):
     @property
     def device_info(self) -> DeviceInfo:
         """Return device information."""
-        return {
-            "identifiers": {(DOMAIN, self._device_id)},
-            "name": self._instance_name,
-            "manufacturer": HYPERHDR_MANUFACTURER_NAME,
-            "model": HYPERHDR_MODEL_NAME,
-        }
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._device_id)},
+            manufacturer=HYPERHDR_MANUFACTURER_NAME,
+            model=HYPERHDR_MODEL_NAME,
+            name=self._instance_name,
+        )
 
     def _get_option(self, key: str) -> Any:
         """Get a value from the provided options."""
--- custom_components/hyperhdr/switch.py
+++ custom_components/hyperhdr/switch.py
@@ -32,7 +32,7 @@ from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import slugify
 
@@ -88,7 +88,7 @@ async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
-) -> bool:
+) -> None:
     """Set up a HyperHDR platform from config entry."""
     entry_data = hass.data[DOMAIN][config_entry.entry_id]
     server_id = config_entry.unique_id
@@ -123,11 +123,12 @@ async def async_setup_entry(
             )
 
     listen_for_instance_updates(hass, config_entry, instance_add, instance_remove)
-    return True
 
 
 class HyperHDRComponentSwitch(SwitchEntity):
     """ComponentBinarySwitch switch class."""
+
+    _attr_entity_category = EntityCategory.CONFIG
 
     def __init__(
         self,
@@ -187,12 +188,12 @@ class HyperHDRComponentSwitch(SwitchEntity):
     @property
     def device_info(self) -> DeviceInfo:
         """Return device information."""
-        return {
-            "identifiers": {(DOMAIN, self._device_id)},
-            "name": self._instance_name,
-            "manufacturer": HYPERHDR_MANUFACTURER_NAME,
-            "model": HYPERHDR_MODEL_NAME,
-        }
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._device_id)},
+            manufacturer=HYPERHDR_MANUFACTURER_NAME,
+            model=HYPERHDR_MODEL_NAME,
+            name=self._instance_name,
+        )
 
     async def _async_send_set_component(self, value: bool) -> None:
         """Send a component control request."""
